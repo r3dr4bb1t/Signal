460aef3e41cf238f2f829c19c23053c789f78b32
redrabbit <redrabbit@hanyang.ac.kr>
2017-11-14 23:34:07 +0900

    how to increase number and sync

--- count.c
+++ count.c
@@ -3,40 +3,49 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#define buf 128
+void sig_usr1(int signo)
+{	
+	char a;
+	sigset_t sigset, oldset;
+	sigemptyset (&oldset);
+	sigemptyset (&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigprocmask(SIG_BLOCK, &sigset, &oldset);
+	
+	fd =open("./sample.txt",O_RDWR);
+	pread (fd, a, sizeof(a));
+	if (argv[0]>a)
+	{a ++;
+	truncate ("./sample.txt", 0);
+	write(fd, a, sizeof(a));
+	}
+}
 int main (int argc, char** argv)
 {	
 	int fd;
-	char buff[buf]={0} ;
-
-	int iszeroOrnewfile=1;
-	int iszero=0;
+	int num=0;
+	struct sigaction usrsig ;
 	if(!(argv[0]>0))
 		printf("insert positive integer");
 	fd = open("./sample.txt",O_RDWR|O_CREAT|O_TRUNC);
-	write(fd, "0",strlen("0");
+	write(fd, num ,sizeof(num);
 	pid_t child[3];
-
-	signal(SIGUSR1, (
-/*	pread ( fd,buff,buf,0);
-	for (int i=0;i<buf;i++)
-	{	
-		if((0 != buff[i])||('0'!=buff[i]) )
-				iszeroOrnewfile = 0;		
-		if('0' == buff [i])
-			 iszero = 1;
-		
-	}
-	if (iszeroOrnewfile == 1 )
-	{ 
-		if (iszero !=1)
-		write(fd, "0",strlen("0"));
-	}
-	else if(iszeroOrnewfile ==0)
-	{
-		truncate ("./sample.txt" , 0);
-		write(fd, "0",strlen("0"));
-	}*/
+					
+	usrsig.sa_handler =sig_usr;  // Parent
+	sigemptyset(&usrsig.sa_mask); 
+ 	usrsig1.sa_flags = 0;
+	sigaction(SIGUSR1,&usrsig, 0);
 	
+	for ( i=0; i<3; i++)
+	{
+	 	child[i] = fork();
+	 	if(child[i] == 0)
+	 	break;
+	}
+	pid_t prev;
+	if(i ==0)	prev = getppid();
+	else 		prev = child[i-1];
+
+	kill(pid_prev, SIGUSR1)
 }
 
