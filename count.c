41925fa5005b6f9555c571c0bd4d1ddadeef8db4
redrabbit <redrabbit@hanyang.ac.kr>
2017-11-16 02:51:03 +0900

    how to sync?

--- count.c
+++ count.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,22 +7,31 @@
 #include <signal.h>
 #include <setjmp.h>
 #include <fcntl.h>
+#include <pthread.h>
 #define buf 10
+
+pthread_mutex_t lock;
+
 void sig_usr (int signo)
 {
-/*	sigset_t sigset;
+	/*sigset_t sigset;
 	sigemptyset(&sigset);
 	sigaddset(&sigset, SIGUSR1);
 	sigprocmask(SIG_BLOCK,&sigset, NULL);
-	
-	*/printf("%d\n",getpid());
+
+	sigwait(&sigset,&signo);*/
+	printf("%d\n",getpid());
+//	sigprocmask(SIG_UNBLOCK,&sigset,NULL);
 
 //	longjmp(jumpbuffer,1);
 }
 
 int main(int argc, char **argv)
 {	int fd;
+	pthread_mutex_init(&lock,NULL);
 	int count = 0;
+	FILE *fp;
+	int i;
 	char buff[buf];
 	struct sigaction usrsig;
 	usrsig.sa_handler =sig_usr;
@@ -30,12 +40,10 @@ int main(int argc, char **argv)
 	sigaction(SIGUSR1,&usrsig,0);
 	if(!(argv[1]>0))
 		printf("insert positive integer");
+
 	fd = open(argv[2],O_RDWR|O_CREAT|O_TRUNC);
-	pwrite(fd,"0",3,0);
-	read(fd,buff,10);
-	printf("initial%s\n",buff);
-	
-//	jmp_buf jumpbuffer;
+	pwrite(fd,"0",1,0);
+
 	pid_t next,ppid,granmaduh;
 	granmaduh = getpid();
 	if ((next = fork()) <0 )
@@ -49,28 +57,40 @@ int main(int argc, char **argv)
 		next = fork();
 		if (next >0)
 		{
-			sleep(6);
+			sleep(2);
 		}
 		else 
 		{
 			next = granmaduh;	
-			sleep(10);
+			sleep(3);
 		}
+	sleep(1);
 	}
 	//setjmp(jumpbuffer);
-	printf("%s",buff);
 	while(1)		
-	{
-		//write(fd,buff[0],sizeof(buf));
-		sleep(1);
+	{	int signo; 
+		int status;
+		sigset_t sigset;
+		sigemptyset(&sigset);
+		sigaddset(&sigset, SIGUSR1);
+		sigprocmask(SIG_BLOCK,&sigset, NULL);	
+		pthread_mutex_lock(&lock);
+		if (i<argv[1])
+		read(fd,buff,10);
+//		printf("initial%c\n",buff[0]);
+		i = atoi(buff);
+ 		++i;
+		snprintf(buff,buf,"%d",i);
+		fd = open(argv[2],O_RDWR|O_CREAT|O_TRUNC);
+		write(fd,buff,1);
 		kill(next, SIGUSR1);
-		printf("%s",buff);
-		pause();
-		sleep(1);
-		
+		pause();	
+		printf("%c\n",buff[0]);
+		pthread_mutex_unlock(&lock);
+		sleep(1);	
 	}
 //	while(1)
 //	{}
 
 	
-	}
+}
