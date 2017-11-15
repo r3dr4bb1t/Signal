85070c6fb0b954c6014fd82a71a2949981517156
redrabbit <redrabbit@hanyang.ac.kr>
2017-11-15 17:01:27 +0900

    p3->p1 problem

--- count.c
+++ count.c
@@ -1,51 +1,52 @@
+#include <errno.h>
 #include <stdio.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-void sig_usr1(int signo)
-{	
-	char a;
-	sigset_t sigset, oldset;
-	sigemptyset (&oldset);
-	sigemptyset (&sigset);
-	sigaddset(&sigset, SIGUSR1);
-	sigprocmask(SIG_BLOCK, &sigset, &oldset);
-	
-	fd =open("./sample.txt",O_RDWR);
-	pread (fd, a, sizeof(a));
-	if (argv[0]>a)
-	{a ++;
-	truncate ("./sample.txt", 0);
-	write(fd, a, sizeof(a));
-	}
+#include <unistd.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <fcntl.h>
+void sig_usr (int signo)
+{
+	printf("%d\n",getpid());
+//	longjmp(jumpbuffer,1);
 }
-int main (int argc, char** argv)
-{	
-	int fd;
-	int num=0;
-	struct sigaction usrsig ;
-	if(!(argv[0]>0))
-		printf("insert positive integer");
-	fd = open("./sample.txt",O_RDWR|O_CREAT|O_TRUNC);
-	write(fd, num ,sizeof(num);
-	pid_t child[3];
-					
-	usrsig.sa_handler =sig_usr;  // Parent
-	sigemptyset(&usrsig.sa_mask); 
- 	usrsig1.sa_flags = 0;
-	sigaction(SIGUSR1,&usrsig, 0);
-	
-	for ( i=0; i<3; i++)
+
+int main(int argc, char **argv)
+{	int fd;
+	fd = open(argv[2],O_RDWR|O_CREAT|O_TRUNC);
+	write(fd,"0",1);
+	signal(SIGUSR1, sig_usr);
+	jmp_buf jumpbuffer;
+	pid_t next,ppid,granmaduh;
+	granmaduh = getpid();
+	if ((next = fork()) <0 )
+	{ 
+			fprintf(stderr, "fork() error");
+			exit(1);
+	 }
+	else if (next == 0  )
 	{
-	 	child[i] = fork();
-	 	if(child[i] == 0)
-	 	break;
+		
+		next = fork();
+		if (next >0)
+		{
+			sleep(6);
+		}
+		else 
+		{
+			signal(SIGUSR1, sig_usr);
+			next = granmaduh;	
+			sleep(10);
+		}
 	}
-	pid_t prev;
-	if(i ==0)	prev = getppid();
-	else 		prev = child[i-1];
+	sleep(2);
+	printf("%d\n",next);
+	//setjmp(jumpbuffer);
 
-	kill(pid_prev, SIGUSR1)
+
+	kill(next, SIGUSR1);
+	exit(1);
+	
 }
-
+	
